122d0cff330009b96a3477971a200b9ae21189be
dhyoga <ystrokeblood@gmail.com>
2023-04-03 08:56:12 +0700

    Tugas

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:learn_login/login_page.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
+    );
+  }
+}
---
+++ lib/register_page.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class RegisterPage extends StatelessWidget {
+  var anime;
+}
